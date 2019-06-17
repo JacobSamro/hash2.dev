ec2d9feb8885255aa00087f7a5d52fd55c12b888
Jacob Samro <jacobsamro@outlook.com>
2019-06-17 17:40:24 +0530

    Prettier Tools Implemented

--- src/services/core/prettier.service.ts
+++ src/services/core/prettier.service.ts
@@ -1,20 +1,57 @@
 
+const beautifyJS = require('js-beautify').js
+const beautifyCSS = require('js-beautify').css
+const beautifyHTML = require('js-beautify').html
 export class PrettierService {
 
   static prettifyHTML(str) {
-
+    return beautifyHTML(str)
   }
 
   static prettifyCSS(str) {
-
+    return beautifyCSS(str)
   }
 
   static prettifyJSON(str) {
-
+    return JSON.stringify(JSON.parse(str), null, 2)
   }
 
-  static prettifyXML(str) {
+  static prettifyXML(xml) {
+    // Source: https://gist.github.com/kurtsson/3f1c8efc0ccd549c9e31
 
+    var formatted = '';
+    var reg = /(>)(<)(\/*)/g;
+    xml = xml.toString().replace(reg, '$1\r\n$2$3');
+    var pad = 0;
+    var nodes = xml.split('\r\n');
+    for (var n in nodes) {
+      var node = nodes[n];
+      var indent = 0;
+      if (node.match(/.+<\/\w[^>]*>$/)) {
+        indent = 0;
+      } else if (node.match(/^<\/\w/)) {
+        if (pad !== 0) {
+          pad -= 1;
+        }
+      } else if (node.match(/^<\w[^>]*[^\/]>.*$/)) {
+        indent = 1;
+      } else {
+        indent = 0;
+      }
+
+      var padding = '';
+      for (var i = 0; i < pad; i++) {
+        padding += '  ';
+      }
+
+      formatted += padding + node + '\r\n';
+      pad += indent;
+    }
+    return formatted.replace(/&/g, '&').replace(/</g, '<').replace(/>/g, '>').replace(/ /g, ' ');
+  }
+
+  static prettifyJS(str) {
+    return beautifyJS(str)
   }
 
 }
