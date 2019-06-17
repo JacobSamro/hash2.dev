6a11a5930c61a7db85b53cfecc2c912e0635d003
Jacob Samro <jacobsamro@outlook.com>
2019-06-17 16:30:40 +0530

    Update Auto Deploy Script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,14 @@ echo "Building Project"
 npx ng build --prod
 git checkout gh-pages
 cp -R dist/browser/ .
-git commit -am "Auto Deploy Script - New Release"
-echo "Pushing new release to Server";
-git push origin gh-pages
+echo "Checking for changes in git"
+if git diff-index --quiet HEAD --; then
+  git commit -am "Auto Deploy Script - New Release"
+  echo "Pushing new release to Server";
+  git push origin gh-pages
+else
+  echo "No changes"
+fi
+
 echo "Finishing";
 git checkout master
