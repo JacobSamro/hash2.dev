0b7ac87d30df21b40e27526ef1a8c7ebf17d59a8
Jacob Samro <jacobsamro@outlook.com>
2019-06-20 20:28:51 +0530

    Implement routing to tools

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: AppComponent,
+    path: "tool/:toolName"
+  }, {
+    component: AppComponent,
+    path: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
