6fad97562c1eafcbecf361bfe87cb80cf095f220
Jacob Samro <jacobsamro@outlook.com>
2019-06-24 17:06:02 +0530

    Fix : Issues in Auto Deploy Script

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,8 @@ echo "Building Project"
 npx ng build --prod
 git checkout gh-pages
 cp -R dist/browser/ .
-echo "Checking for changes in git"
-if git diff-index --quiet HEAD --; then
-  git commit -am "Auto Deploy Script - New Release"
-  echo "Pushing new release to Server";
-  git push origin gh-pages
-else
-  echo "No changes"
-fi
-
+git commit -am "Auto Deploy Script - New Release"
+echo "Pushing new release to Server";
+git push origin gh-pages
 echo "Finishing";
 git checkout master
