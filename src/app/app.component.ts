0c3fb539ba34b855b500875024f57f9b58f632e6
Jacob Samro <jacobsamro@outlook.com>
2019-06-25 17:00:50 +0530

    Fix: jQuery Modal Issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProviderService } from 'src/services/provider.service';
 import { ActivatedRoute, Router, Params } from '@angular/router';
-
-declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -60,7 +58,6 @@ export class AppComponent implements OnInit {
     this.modalButton = t.button;
     this.selectedSubTool = 0;
     this.currentToolIndex = index
-    $('#myModal').modal('show');
     this.router.navigate(['/tool/', t.u])
   }
 
