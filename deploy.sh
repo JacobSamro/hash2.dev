fc50541c93d9e299ff8d23f752302ce1f951fc6f
Jacob Samro <jacobsamro@outlook.com>
2019-07-08 12:48:56 +0530

    Custom 404 fallback to handle direct access tool page

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ echo "Building Project"
 npx ng build --prod
 git checkout gh-pages
 cp -R dist/browser/ .
+cp index.html 404.html
 git commit -am "Auto Deploy Script - New Release"
 echo "Pushing new release to Server";
 git push origin gh-pages
