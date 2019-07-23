aca44d480d3191856beab9ea71ebddeb39698762
Jacob Samro <jacobsamro@outlook.com>
2019-07-23 13:38:52 +0530

    Fix: Incorrect argument on git commit

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git checkout gh-pages
 cp -R dist/browser/ .
 cp index.html 404.html
 git add .
-git commit m "Auto Deploy Script - New Release"
+git commit -m "Auto Deploy Script - New Release"
 echo "Pushing new release to Server";
 git push origin gh-pages
 echo "Finishing";
