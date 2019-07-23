26f937a9e0d20f01387ca558ebe21276b19bede5
Jacob Samro <jacobsamro@outlook.com>
2019-07-23 13:38:08 +0530

    Fix commit issue on the AutoDeploy Script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ npx ng build --prod
 git checkout gh-pages
 cp -R dist/browser/ .
 cp index.html 404.html
-git commit -am "Auto Deploy Script - New Release"
+git add .
+git commit m "Auto Deploy Script - New Release"
 echo "Pushing new release to Server";
 git push origin gh-pages
 echo "Finishing";
